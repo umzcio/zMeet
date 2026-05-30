623dc4599b9083e6ae16a04f914051fcdc5ae5e7
umzcio <umzcio@users.noreply.github.com>
2026-05-30 16:48:42 -0600

    v1.6.0: full-text meeting search (Phase 7)

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -12,8 +12,8 @@ RES_DIR="$APP_DIR/Contents/Resources"
 FW_DIR="$APP_DIR/Contents/Frameworks"
 
 # App version (also stamped into the appcast on release).
-VERSION="1.5.1"
-BUILD="3"
+VERSION="1.6.0"
+BUILD="4"
 
 # Sparkle auto-update: appcast feed URL + EdDSA public key (private key is in the
 # login keychain via generate_keys).
