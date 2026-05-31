4067a3d0529b87f2f10bad9dc708e7ea74cb6da1
umzcio <umzcio@users.noreply.github.com>
2026-05-30 21:26:57 -0600

    v1.8.0: rail right-click context menu

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -12,8 +12,8 @@ RES_DIR="$APP_DIR/Contents/Resources"
 FW_DIR="$APP_DIR/Contents/Frameworks"
 
 # App version (also stamped into the appcast on release).
-VERSION="1.7.0"
-BUILD="5"
+VERSION="1.8.0"
+BUILD="6"
 
 # Sparkle auto-update: appcast feed URL + EdDSA public key (private key is in the
 # login keychain via generate_keys).
