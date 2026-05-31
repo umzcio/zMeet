d6252d2b2c16307c812f5998f280cc7951a68c8b
umzcio <umzcio@users.noreply.github.com>
2026-05-31 16:23:28 -0600

    Bump version to 1.9.0 (build 9)

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -12,8 +12,8 @@ RES_DIR="$APP_DIR/Contents/Resources"
 FW_DIR="$APP_DIR/Contents/Frameworks"
 
 # App version (also stamped into the appcast on release).
-VERSION="1.8.2"
-BUILD="8"
+VERSION="1.9.0"
+BUILD="9"
 
 # Sparkle auto-update: appcast feed URL + EdDSA public key (private key is in the
 # login keychain via generate_keys).
