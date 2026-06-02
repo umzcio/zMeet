fb88702ba44c3a62426394b4dde3047d54c22732
umzcio <umzcio@users.noreply.github.com>
2026-06-02 10:25:51 -0600

    Bump version to 1.13.1 (build 18)

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -12,8 +12,8 @@ RES_DIR="$APP_DIR/Contents/Resources"
 FW_DIR="$APP_DIR/Contents/Frameworks"
 
 # App version (also stamped into the appcast on release).
-VERSION="1.13.0"
-BUILD="17"
+VERSION="1.13.1"
+BUILD="18"
 
 # Sparkle auto-update: appcast feed URL + EdDSA public key (private key is in the
 # login keychain via generate_keys).
